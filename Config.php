7acfa99b1e2fd281ad686c14496f0b29180f676e
Naman Aggarwal <naman.aggarwal@yahoo.com>
2014-07-08 17:46:43 +0800

    Create Config.php

---
+++ Config.php
@@ -0,0 +1,11 @@
+<?php
+
+class config{
+
+	const DB_NAME = "carddb";
+	const USER_TABLE = "users";	
+	const BASE_URL = "/CardAppBackend";
+	const HOST = "localhost";
+	const DB_USERNAME = "root";
+	const DB_PASSWORD = "";
+};
